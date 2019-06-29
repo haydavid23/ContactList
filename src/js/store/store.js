deeb00d6bbbbb3fa0dd7915a234b0b2221ef0b1a
David Hay <hay.david@hotmail.com>
2019-06-29 22:44:38 +0000

    new

--- src/js/store/store.js
+++ src/js/store/store.js
@@ -1,55 +1,56 @@
 import PropTypes from "prop-types";
 import React from "react";
 
+
 const getState = ({ getStore, setStore }) => {
-	
+
 	return {
-		store: 
+		store:
 
 		{
 			contacts:[],
 			id:""
-			
-		
-			
+
+
+
 		},
-		
-		
-		
+
+
+
 		actions: {
 		deleteCon: (id,close)=>{
 			const store = getStore();
 				fetch("https://assets.breatheco.de/apis/fake/contact/" + id,{
 					method: "DELETE"
-					
 
-			
+
+
 				})
-					
+
 				.then(getDataUpdated=>  {
 				fetch("https://assets.breatheco.de/apis/fake/contact/agenda/downtown_viii")
 				.then(response => response.json())
 
 				.then(data => {
-			
+
 					store.contacts = data;
 					setStore({ store:store });
-		
-				
+
+
 				});
 				});
-				
+
 			close;
-				
+
 		},
-		
-			
-		
-		
+
+
+
+
 		addContact: (name,email,number,address,props) => {
 			const store = getStore();
 				fetch("https://assets.breatheco.de/apis/fake/contact/",{
-					method: 'POST', 
+					method: 'POST',
 				headers:{ 'Content-Type': 'application/json' },
 				body: JSON.stringify({
 					"full_name":name,
@@ -57,35 +58,35 @@ const getState = ({ getStore, setStore }) => {
 					"phone":number,
 					"address":address,
 					"agenda_slug":"downtown_viii"
-					
+
 				})
 				})
-				
-				
+
+
 				.then(getDataUpdated=>  {
 				fetch("https://assets.breatheco.de/apis/fake/contact/agenda/downtown_viii")
 				.then(response => response.json())
 
 				.then(data => {
-			
+
 					store.contacts = data;
 					setStore({ store:store});
-		
-				
+
+
 				});
 				});
-				
+
 				props;
 
-			
 
-				
+
+
 				},
-		
+
 			editContact: (name,email,number,address,id,props) => {
 			const store = getStore();
 				fetch("https://assets.breatheco.de/apis/fake/contact/" + id  ,{
-					method: 'PUT', 
+					method: 'PUT',
 				headers:{ 'Content-Type': 'application/json' },
 				body: JSON.stringify({
 					"full_name":name,
@@ -93,43 +94,43 @@ const getState = ({ getStore, setStore }) => {
 					"phone":number,
 					"address":address,
 					"agenda_slug":"downtown_viii"
-					
-					
-					
+
+
+
 				})
 				})
-				
+
 				.then(getDataUpdated=>  {
 				fetch("https://assets.breatheco.de/apis/fake/contact/agenda/downtown_viii")
 				.then(response => response.json())
 
 				.then(data => {
-			
+
 					store.contacts = data;
 					setStore({ store:store });
-		
-				
+
+
 				});
 				});
-				
+
 				props;
 			}
-			
-			
-				
+
+
+
 				}
 
-				
-				
-			
-				
+
+
+
+
 			};
 		};
-		
+
 getState.propTypes = {
 		history: PropTypes.object,
 		match:	PropTypes.object
-		
+
 };
 
 export default getState;
